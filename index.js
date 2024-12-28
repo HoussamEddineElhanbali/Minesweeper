4a679621180b60bd6db09497cb08a4ae3289b64f
Houssam Eddine <houssameddineel@gmail.com>
2024-12-28 15:52:40 +0100

    changing logic

--- index.js
+++ index.js
@@ -2,6 +2,13 @@
 
 let board = document.querySelector(".board");
 let gridBoard = [];
+let difficulty = 6;
+let bombs = [];
+let surroundingPattern = [
+    [-1,-1],[-1,0],[-1,1]
+    ,[0,-1],        [0,1]
+    ,[1,-1],[1,0],[1,1]
+];
 
 function intialize()
 {
@@ -31,39 +38,90 @@ function intialize()
         }
 }
 
+function generateBombs()
+{
+    for(let i = 0; i < difficulty; i++)
+    {
+        let bomb = [];
+
+        do
+        {
+            bomb[0] = Math.floor(Math.random() * 10);
+            bomb[1] = Math.floor(Math.random() * 10);
+        }
+        while(checkDuplicates(bomb));
+
+        bombs.push([bomb[0],bomb[1]]);
+    }
+
+    bombs.forEach(Element => 
+    {
+        gridBoard[Element[0]][Element[1]] = -1;
+    });
+}
+
+function checkDuplicates(bomb)
+{
+    for(let i = 0; i < bombs.length; i++)
+    {
+        if(bomb[0] === bombs[i][0] && bomb[1] === bombs[i][1])
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+function generateSurroundingNumbers()
+{
+    bombs.forEach(Element => 
+    {
+        surroundingPattern.forEach(pattern => 
+        {
+            let collumn = Element[0] + pattern[0];
+            let row = Element[1] + pattern[1];
+            if( collumn >= 0 && collumn < 10 && row >= 0 && row < 10)
+            {
+                if(gridBoard[collumn][row] >= 0)
+                {
+                    gridBoard[Element[0]+pattern[0]][Element[1]+pattern[1]] += 1;
+                }
+            }
+        });
+    });
+}
+
 function detectClick(event)
 {
     event.target.className = "cellTwo";
 }
 
-let bombs = [];
 
-function generateBombs()
+function revealAll()
 {
-    let bomb;
-
-    bomb = fillBomb();
-
-    bombs.forEach(Element => 
+    for(let i = 0; i < 10; i++)
     {
-        while(Element.x === bomb.x && Element.y === bomb.y)
+        for(let j = 0; j < 10; j++)
+        {
+            if(gridBoard[j][i] >= 0)
             {
-                bomb = fillBomb();
+                let newCell = document.createElement("div");
+                newCell.className = "cellTwo";
+                newCell.style.gridColumn = i + 1;
+                newCell.style.gridRow = j + 1;
+                if(gridBoard[j][i] > 0)
+                {
+                    newCell.innerHTML = gridBoard[j][i];
+                }
+                board.appendChild(newCell);
             }
-    });
-
-    bombs.push(bomb);
+        }
+    }
 }
 
-function fillBomb()
-{
-    let x = Math.floor((Math.random() * 10) + 1);
-    let y = Math.floor((Math.random() * 10) + 1);
-    
-    return {x,y};
-}
 
 intialize();
-
 generateBombs();
-console.log(bombs);
+generateSurroundingNumbers();
+revealAll();
+console.log(gridBoard);
